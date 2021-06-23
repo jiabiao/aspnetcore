13c203ac61fe5fdbb3377f60ca44987457c92e1d
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2021-06-23 14:25:15 +0100

    Add QueryStringEnumerable

---
+++ src/Components/Components/src/Routing/QueryStringEnumerable.cs
@@ -0,0 +1,104 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNetCore.Components.Routing
+{
+    // A mechanism for reading key/value pairs from a querystring without having
+    // to allocate and populate an entire dictionary
+    internal readonly struct QueryStringEnumerable
+    {
+        private readonly string _queryString;
+
+        public QueryStringEnumerable(string queryString)
+        {
+            _queryString = queryString;
+        }
+
+        public Enumerator GetEnumerator()
+            => new Enumerator(_queryString);
+
+        public struct Enumerator
+        {
+            private readonly string queryString;
+            private readonly int textLength;
+            private int scanIndex;
+            private int equalIndex;
+            private string? currentName;
+            private string? currentValue;
+
+            public Enumerator(string? query)
+            {
+                if (string.IsNullOrEmpty(query))
+                {
+                    this = default;
+                    queryString = string.Empty;
+                }
+                else
+                {
+                    currentName = null;
+                    currentValue = null;
+                    queryString = query;
+                    scanIndex = queryString[0] == '?' ? 1 : 0;
+                    textLength = queryString.Length;
+                    equalIndex = queryString.IndexOf('=');
+                    if (equalIndex == -1)
+                    {
+                        equalIndex = textLength;
+                    }
+                }
+            }
+
+            public (string key, string value) Current
+                => (currentName!, currentValue!);
+
+            public bool MoveNext()
+            {
+                currentName = null;
+                currentValue = null;
+
+                if (scanIndex < textLength)
+                {
+                    var delimiterIndex = queryString.IndexOf('&', scanIndex);
+                    if (delimiterIndex == -1)
+                    {
+                        delimiterIndex = textLength;
+                    }
+
+                    if (equalIndex < delimiterIndex)
+                    {
+                        while (scanIndex != equalIndex && char.IsWhiteSpace(queryString[scanIndex]))
+                        {
+                            ++scanIndex;
+                        }
+
+                        var name = queryString.Substring(scanIndex, equalIndex - scanIndex);
+                        var value = queryString.Substring(equalIndex + 1, delimiterIndex - equalIndex - 1);
+                        currentName = Uri.UnescapeDataString(name.Replace('+', ' '));
+                        currentValue = Uri.UnescapeDataString(value.Replace('+', ' '));
+
+                        equalIndex = queryString.IndexOf('=', delimiterIndex);
+                        if (equalIndex == -1)
+                        {
+                            equalIndex = textLength;
+                        }
+                    }
+                    else
+                    {
+                        if (delimiterIndex > scanIndex)
+                        {
+                            var name = queryString.Substring(scanIndex, delimiterIndex - scanIndex);
+                            currentName = Uri.UnescapeDataString(name.Replace('+', ' '));
+                            currentValue = string.Empty;
+                        }
+                    }
+
+                    scanIndex = delimiterIndex + 1;
+                }
+
+                return currentName is not null;
+            }
+        }
+    }
+}
---
+++ src/Components/Components/test/Routing/QueryStringEnumerableTest.cs
@@ -0,0 +1,114 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Components.Routing
+{
+    public class QueryStringEnumerableTest
+    {
+        [Fact]
+        public void ParseQueryWithUniqueKeysWorks()
+        {
+            Assert.Collection(Parse("?key1=value1&key2=value2"),
+                kvp => AssertKeyValuePair("key1", "value1", kvp),
+                kvp => AssertKeyValuePair("key2", "value2", kvp));
+        }
+
+        [Fact]
+        public void ParseQueryWithoutQuestionmarkWorks()
+        {
+            Assert.Collection(Parse("key1=value1&key2=value2"),
+                kvp => AssertKeyValuePair("key1", "value1", kvp),
+                kvp => AssertKeyValuePair("key2", "value2", kvp));
+        }
+
+        [Fact]
+        public void ParseQueryWithDuplicateKeysGroups()
+        {
+            Assert.Collection(Parse("?key1=valueA&key2=valueB&key1=valueC"),
+                kvp => AssertKeyValuePair("key1", "valueA", kvp),
+                kvp => AssertKeyValuePair("key2", "valueB", kvp),
+                kvp => AssertKeyValuePair("key1", "valueC", kvp));
+        }
+
+        [Fact]
+        public void ParseQueryWithEmptyValuesWorks()
+        {
+            Assert.Collection(Parse("?key1=&key2="),
+                kvp => AssertKeyValuePair("key1", string.Empty, kvp),
+                kvp => AssertKeyValuePair("key2", string.Empty, kvp));
+        }
+
+        [Fact]
+        public void ParseQueryWithEmptyKeyWorks()
+        {
+            Assert.Collection(Parse("?=value1&="),
+                kvp => AssertKeyValuePair(string.Empty, "value1", kvp),
+                kvp => AssertKeyValuePair(string.Empty, string.Empty, kvp));
+        }
+
+        [Fact]
+        public void ParseQueryWithEncodedKeyWorks()
+        {
+            Assert.Collection(Parse("?fields+%5BtodoItems%5D"),
+                kvp => AssertKeyValuePair("fields [todoItems]", string.Empty, kvp));
+        }
+
+        [Fact]
+        public void ParseQueryWithEncodedValueWorks()
+        {
+            Assert.Collection(Parse("?=fields+%5BtodoItems%5D"),
+                kvp => AssertKeyValuePair(string.Empty, "fields [todoItems]", kvp));
+        }
+
+        [Fact]
+        public void ParseQueryWithEncodedKeyEmptyValueWorks()
+        {
+            Assert.Collection(Parse("?fields+%5BtodoItems%5D="),
+                kvp => AssertKeyValuePair("fields [todoItems]", string.Empty, kvp));
+        }
+
+        [Fact]
+        public void ParseQueryWithEncodedKeyEncodedValueWorks()
+        {
+            Assert.Collection(Parse("?fields+%5BtodoItems%5D=%5B+1+%5D"),
+                kvp => AssertKeyValuePair("fields [todoItems]", "[ 1 ]", kvp));
+        }
+
+        [Fact]
+        public void ParseQueryWithEncodedKeyEncodedValuesWorks()
+        {
+            Assert.Collection(Parse("?fields+%5BtodoItems%5D=%5B+1+%5D&fields+%5BtodoItems%5D=%5B+2+%5D"),
+                kvp => AssertKeyValuePair("fields [todoItems]", "[ 1 ]", kvp),
+                kvp => AssertKeyValuePair("fields [todoItems]", "[ 2 ]", kvp));
+        }
+
+        [Theory]
+        [InlineData("?")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ParseEmptyOrNullQueryWorks(string queryString)
+        {
+            Assert.Empty(Parse(queryString));
+        }
+
+        private static void AssertKeyValuePair(string expectedKey, string expectedValue, (string key, string value) actual)
+        {
+            Assert.Equal(expectedKey, actual.key);
+            Assert.Equal(expectedValue, actual.value);
+        }
+
+        private static IReadOnlyList<(string key, string value)> Parse(string query)
+        {
+            var result = new List<(string key, string value)>();
+            foreach (var (key, value) in new QueryStringEnumerable(query))
+            {
+                result.Add((key, value));
+            }
+
+            return result;
+        }
+    }
+}
